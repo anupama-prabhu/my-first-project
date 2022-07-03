f5a73a84e2a73b5f95e717af3c8b57f6be5f6c64
anupama-prabhu <101841267+anupama-prabhu@users.noreply.github.com>
2022-07-03 19:28:55 -0400

    Changed the background color etc.

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 export const Home = () => {
     return (
-        <h1>This is my home component</h1>
+        <div class="timings">
+            <div class="booking"> Reservarions </div>
+            <p>Wednesday | Thursday | Sunday : 5.30 - 9.30PM</p>
+            <p> Friday &amp; Saturday : 5.00 - 10.00 pm </p>
+            <p>Sunday Lunch : 12.30 - 1.30 pm </p>
+            <p>Closed Monday &amp; Tuesday</p>
+        </div>
     )
 }
 
